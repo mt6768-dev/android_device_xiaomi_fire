01ec1aa004038c8417d896c06a2b10fcf021baa0
Bruno Martins <bgcngm@gmail.com>
2025-01-08 14:38:40 +0100

    fire: Enable smooth display with 90Hz
    There's no point to keep using custom pref to set minimum refresh rate
    when AOSP smooth display feature behaves in the exact same way in
    our case.
    
    Change-Id: I497918a9c9687443a9438fb6291769f6e51d9379

--- device.mk
+++ device.mk
@@ -157,6 +157,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayFire \
     FrameworksResOverlayFire \
+    SettingsOverlayFire \
     SystemUIOverlayFire \
     TelephonyOverlayFire \
     WifiResOverlayFire
