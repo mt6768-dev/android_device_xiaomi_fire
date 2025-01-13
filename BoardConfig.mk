83714d9c9aef67436520219d1b251870913bfb22
Hadad <hadad@linuxmail.org>
2025-01-13 10:12:17 +0100

    fire: Include Xiaomi common FCM.
    Change-Id: I11a95944bc80d476750afcde9d5b4506344ce5bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
