c1c081808294c69f27280454989c9fefac4fd849
Matsvei Niaverau <matvej2002@gmail.com>
2026-04-01 18:42:45 +0200

    fire: Rename memtrack implementation
    Change-Id: I8258d4ae7015dd8d303393ee32e4b3a5bf9acfbf

--- device.mk
+++ device.mk
@@ -88,7 +88,7 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.memtrack-service.mediatek-mali \
+    android.hardware.memtrack-service.mediatek
 
 # DRM
 PRODUCT_PACKAGES += \
