212612851e286891b87388e5895bcce63f26e67d
claxten10 <claxten10@gmail.com>
2026-05-26 18:37:04 +0200

    fire: Build dummy libmtkperf_client_vendor
    Change-Id: I7b63626042d4ae1389a322f218294cd25416291a

--- device.mk
+++ device.mk
@@ -215,7 +215,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
-    libmtkperf_client
+    libmtkperf_client \
+    libmtkperf_client_vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
