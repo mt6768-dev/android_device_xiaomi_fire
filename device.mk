f3bfd0e6f4302b5dac8dfb092e363a202c5c74e4
Mashopy <eliasgheeraert@gmail.com>
2026-07-06 18:34:04 +0200

    fire: Switch to common mediatek logtag properties
    Change-Id: Ie9ab821ec0d8e370215950029c0ed75699651ea4

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
     PowerOffAlarm
 
 # Properties
-include $(LOCAL_PATH)/vendor_logtag.mk
+include hardware/mediatek/configs/properties/vendor_logtag.mk
 
 # Rootdir
 PRODUCT_PACKAGES += \
